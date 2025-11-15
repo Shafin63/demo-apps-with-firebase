2eb5144f39760d4f29ddf5204bf959ec8bb45dad
Shafin <silentsk456@gmail.com>
2025-11-15 14:26:21 +0600

    realtime data fetched

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:live_score_app_with_firebase/home_screen.dart';
+
+class FootballLiveScoreApp extends StatelessWidget {
+  const FootballLiveScoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
